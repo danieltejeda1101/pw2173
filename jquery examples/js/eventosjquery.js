766d50d95685e8008ef979a9a38fb4875709bbe2
danieltejeda1101 <thedanixfun@gmail.com>
2017-11-09 15:57:20 -0700

    Perfil de randomuser

--- jquery examples/js/eventosjquery.js
+++ jquery examples/js/eventosjquery.js
@@ -2,18 +2,29 @@
 
 
 var inicia = function(){
-	//alert("Lista la página");
-	//JSON = JavaScript Object Notation
-	$.ajax({
-		url: 'https://randomuser.me/api/',
-		dataType: 'json',
-		success: function(data) {
-			alert(data.results[0].name.first +" "+ data.results[0].name.last);
-		}
-	});
+	
+	var nuevo = function(){
+		//JSON = JavaScript Object Notation
+		$.ajax({
+			url: 'https://randomuser.me/api/',
+			dataType: 'json',
+			success: function(data) {
+				//Llenar información
+				$("#nombre").html(data.results[0].name.first+" "+data.results[0].name.last);
+				$("#email").html(data.results[0].email);
+				$("#cumpleaños").html(data.results[0].dob);
+				$("#foto").attr("src",data.results[0].picture.large);
+			}
+		});
+	}
 
+	nuevo();
+	$("#btnNuevo").on("click",nuevo);
 
 }
+
+
+
 
 $(document).ready(inicia);
 
