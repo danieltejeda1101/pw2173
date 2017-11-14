55e89e2dc6705c627d7b17c0ab2a267e82be70c8
danieltejeda1101 <thedanixfun@gmail.com>
2017-11-14 16:09:04 -0700

    Electron

---
+++ calculadora2/main.js
@@ -0,0 +1,25 @@
+//Necesitamos estas cosas
+const app = require('electron').app;
+const BrowserWindow = require('electron').BrowserWindow;
+const path = require('path'); //Muestra la ruta del archivo
+const url = require('url');//Carga una url
+
+// ECMASCRIPT = 6
+// Constante sin valor inicial
+let PantallaPrincipal;
+
+//Función
+function muestraPantallaPrincipal(){
+	PantallaPrincipal = new BrowserWindow({width:320,height:425});
+	PantallaPrincipal.loadURL(url.format({
+		pathname: path.join(__dirname,'index.html'),
+		protocol: 'file',
+		slashes: true
+	}))
+
+	PantallaPrincipal.show();
+}
+
+
+//Encender la aplicación
+app.on('ready', muestraPantallaPrincipal)
